78fc218e5af0a7ab7a91f74ffab2205f95c09836
Harshit <prajapati.ha@northeastern.edu>
2024-02-06 16:05:20 -0500

    first commit

---
+++ src/components/Table/Table.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Table.module.css';
+
+const Table = () => (
+  <div className={styles.Table}>
+    Table Component
+  </div>
+);
+
+Table.propTypes = {};
+
+Table.defaultProps = {};
+
+export default Table;
